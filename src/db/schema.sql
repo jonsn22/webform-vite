2d268b71c657253347d510b6f054d9661527d9d2
jonsn22 <hannes.thurner@gmail.com>
2026-01-22 19:49:26 +0100

    test

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS Todo;
 CREATE TABLE IF NOT EXISTS Todo (id INTEGER PRIMARY KEY, description TEXT, is_done INTEGER NOT NULL DEFAULT 0);
+INSERT INTO Todo (id, description) VALUES (1, 'todo1-Just for testing'), (2, 'todo2'), (3, 'todo3');
